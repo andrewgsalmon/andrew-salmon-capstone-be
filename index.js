174c6d3de32b6a98fd9a6e83fd56c44c7808430e
Andrew Salmon <andrew.g.salmon@gmail.com>
2024-04-16 10:15:00 -0400

    more cors testing

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.use(cors( { origin: CORS_ORIGIN } ));
 
 app.options('*', cors());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', CORS_ORIGIN);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 app.use(express.json());
 
 //SPOTIFY OAUTH IMPLEMENTATION TBD
