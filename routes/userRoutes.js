2447a1bb50c0c6e0e6a6c93d91d60949b361228d
Andrew Salmon <andrew.g.salmon@gmail.com>
2024-07-11 14:37:32 -0400

    refactored express users route for more readability

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
-const knex = require('knex')(require('../knexfile'));
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const authorize = require('../middleware/authorize');
+const knex = require("knex")(require("../knexfile"));
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authorize = require("../middleware/authorize");
 
 // ## POST /api/users/register
 // - Creates a new user.
@@ -14,7 +14,7 @@ router.post("/register", async (req, res) => {
   }
 
   // Create a hashed Password using brcrypt.hashSync(password)
-  const hashedPassword = bcrypt.hashSync(password)
+  const hashedPassword = bcrypt.hashSync(password);
 
   // Create the new user
   const newUser = {
@@ -22,12 +22,12 @@ router.post("/register", async (req, res) => {
     fav_artists,
     location,
     email,
-    password: hashedPassword //update password to use hashed password
+    password: hashedPassword, //update password to use hashed password
   };
 
   // Insert it into our database
   try {
-    await knex('users').insert(newUser);
+    await knex("users").insert(newUser);
     res.status(201).send("Registered successfully");
   } catch (error) {
     res.status(400).send(req.body);
@@ -42,25 +42,26 @@ router.post("/login", async (req, res) => {
     return res.status(400).send("Forget something? Both fields are required!");
   }
 
-  const user = await knex('users').where({ email: email }).first();
+  const user = await knex("users").where({ email: email }).first();
   if (!user) {
-    return res.status(404).send("Hmm... no account exists under that email!  Be sure to register.")
+    return res
+      .status(404)
+      .send("Hmm... no account exists under that email!  Be sure to register.");
   }
 
-  const isPasswordCorrect = bcrypt.compareSync(password, user.password)
+  const isPasswordCorrect = bcrypt.compareSync(password, user.password);
   if (!isPasswordCorrect) {
-    return res.status(401).send("Incorrect password! Let's try that again...")
+    return res.status(401).send("Incorrect password! Let's try that again...");
   }
 
   const token = jwt.sign(
     { id: user.id, email: user.email },
     process.env.JWT_KEY,
-    { expiresIn: '24h' }
-  )
+    { expiresIn: "24h" }
+  );
 
-  res.json({ token: token })
+  res.json({ token: token });
 });
-
 
 // ## GET /api/users/current
 router.get("/current", async (req, res) => {
@@ -69,80 +70,81 @@ router.get("/current", async (req, res) => {
   }
 
   const authHeader = req.headers.authorization;
-  const authToken = authHeader.split(' ')[1];
+  const authToken = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(authToken, process.env.JWT_KEY);
-    const user = await knex('users').where({ id: decoded.id }).first();
+    const user = await knex("users").where({ id: decoded.id }).first();
 
-    delete user.password
+    delete user.password;
     res.json(user);
   } catch (error) {
     return res.status(401).send("Invalid auth token");
   }
 });
 
-
 router.get("/current", authorize, async (req, res) => {
-
   try {
-    const user = await knex('users').where({ id: req.user.id }).first();
+    const user = await knex("users").where({ id: req.user.id }).first();
     res.json(user);
   } catch (error) {
     return res.status(500).send(`Unknown server error: ${error}`);
   }
 });
 
-router.post("/likes", async (req, res) => {
-  const { user_email, artist_name, artist_id, artist_img } = req.body;
+router
+  .route("/likes")
+  .post(async (req, res) => {
+    const { user_email, artist_name, artist_id, artist_img } = req.body;
 
-  // post a new liked artist record associated to a user
-  const newLike = {
-    user_email,
-    artist_name,
-    artist_id,
-    artist_img
-  };
+    // post a new liked artist record associated to a user
+    const newLike = {
+      user_email,
+      artist_name,
+      artist_id,
+      artist_img,
+    };
 
-  // Insert it into our database
-  try {
-    await knex('likes').insert(newLike);
-    res.status(201).send("Nice! We've saved the artist to your profile.");
-  } catch (error) {
-    console.error(error);
-    res.status(400).send(req.body);
-  }
-});
-
-router.get("/likes", async (req, res) => {
-  const { user_email } = req.query;
-
-  try {
-    const likes = await knex('likes').where({ user_email });
-
-    res.json(likes);
-  } catch (error) {
-    return res.status(401).send("Invalid request");
-  }
-});
-
-// Remove a liked playlist
-router.delete('/likes', async (req, res) => {
-  const { user_email, artist_id } = req.query;
-  try {
-    const likedArtist = await knex('likes').where({
-      user_email: user_email,
-      artist_id: artist_id
-    }).delete();
-
-    if (likedArtist > 0) {
-      return res.status(200).send("Successfully deleted!");
-    } else {
-      return res.status(404).send("Hmm. Artist wasn't found in your profile... Strange!");
+    // Insert it into our database
+    try {
+      await knex("likes").insert(newLike);
+      res.status(201).send("Nice! We've saved the artist to your profile.");
+    } catch (error) {
+      console.error(error);
+      res.status(400).send(req.body);
     }
-  } catch (error) {
-    res.status(500).send(`Something went wrong: ${error}`);
-  }
-});
+  })
+  .get(async (req, res) => {
+    const { user_email } = req.query;
+
+    try {
+      const likes = await knex("likes").where({ user_email });
+
+      res.json(likes);
+    } catch (error) {
+      return res.status(401).send("Invalid request");
+    }
+  })
+  .delete(async (req, res) => {
+    const { user_email, artist_id } = req.query;
+    try {
+      const likedArtist = await knex("likes")
+        .where({
+          user_email: user_email,
+          artist_id: artist_id,
+        })
+        .delete();
+
+      if (likedArtist > 0) {
+        return res.status(200).send("Successfully deleted!");
+      } else {
+        return res
+          .status(404)
+          .send("Hmm. Artist wasn't found in your profile... Strange!");
+      }
+    } catch (error) {
+      res.status(500).send(`Something went wrong: ${error}`);
+    }
+  });
 
 module.exports = router;
