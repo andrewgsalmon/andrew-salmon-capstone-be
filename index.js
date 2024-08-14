13e7db47676c6aeb921c1ed0ba7c2a3757f3d5b1
Andrew Salmon <andrew.g.salmon@gmail.com>
2024-08-14 17:45:27 -0400

    fixed bug fix with spotify api call

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const app = express();
 require("dotenv").config();
 const userRoutes = require('./routes/userRoutes');
 const commentRoutes = require('./routes/commentRoutes');
-const spotifyRoutes = require('./routes/spotifyRoutes');
+// const spotifyRoutes = require('./routes/spotifyRoutes');
 const cors = require("cors");
 const PORT = process.env.PORT || 5050;
-const client_id = process.env.CLIENT_ID;
-const client_secret = process.env.CLIENT_SECRET;
-const client_url = process.env.CLIENT_URL;
-const passport = require('passport');
+// const client_id = process.env.CLIENT_ID;
+// const client_secret = process.env.CLIENT_SECRET;
+// const client_url = process.env.CLIENT_URL;
+// const passport = require('passport');
 require('./passport')
-const SpotifyStrategy = require('passport-spotify').Strategy;
+// const SpotifyStrategy = require('passport-spotify').Strategy;
 const { CORS_ORIGIN } = process.env;
 
 app.use(cors( { origin: CORS_ORIGIN } ));
@@ -25,33 +25,32 @@ app.options('*', cors({
 app.use(express.json());
 
 //SPOTIFY OAUTH IMPLEMENTATION TBD
-passport.use(
-  new SpotifyStrategy(
-    {
-      clientID: client_id,
-      clientSecret: client_secret,
-      callbackURL: `${client_url}/home`
-    },
-    function(accessToken, refreshToken, expires_in, profile, done) {
-      User.findOrCreate({ spotifyId: profile.id }, function(err, user) {
-        return done(err, user);
-      });
-    }
-  )
-);
+// passport.use(
+//   new SpotifyStrategy(
+//     {
+//       clientID: client_id,
+//       clientSecret: client_secret,
+//       callbackURL: `${client_url}/home`
+//     },
+//     function(accessToken, refreshToken, expires_in, profile, done) {
+//       User.findOrCreate({ spotifyId: profile.id }, function(err, user) {
+//         return done(err, user);
+//       });
+//     }
+//   )
+// );
 
-app.get('/auth/spotify', passport.authenticate('spotify', {
-  scope: ['user-read-email', 'user-read-private']
-}));
+// app.get('/auth/spotify', passport.authenticate('spotify', {
+//   scope: ['user-read-email', 'user-read-private']
+// }));
 
-app.get(
-  '/auth/spotify/callback',
-  passport.authenticate('spotify', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect(`${client_url}/home`);
-  }
-);
+// app.get(
+//   '/auth/spotify/callback',
+//   passport.authenticate('spotify', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     res.redirect(`${client_url}/home`);
+//   }
+// );
 
 app.get('/api', (req, res) => {
   res.send('Welcome to the API of the Hit Me app!');
@@ -61,6 +60,6 @@ app.use('/api/users', userRoutes);
 
 app.use('/api/artists', commentRoutes);
 
-app.use('/api/spotify', spotifyRoutes);
+// app.use('/api/spotify', spotifyRoutes);
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
