440132d3a9929d1c064da1388379904d5b9bfaf7
Andrew Salmon <andrew.g.salmon@gmail.com>
2024-08-16 15:15:16 -0400

    updated oauth strategy, adding relative path instead of localhost

--- middleware/passport.js
+++ middleware/passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8080/auth/google/callback",
+      callbackURL: "/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
